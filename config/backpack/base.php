eabc79af7844613315dc33aa059c98c790c2c2ef
sameh <smta55@hotmail.com>
2022-04-15 16:16:18 +0200

    fotter

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -137,10 +137,10 @@ return [
     'developer_name' => 'Nile Falcon',
 
     // Developer website. Link in footer. Type false if you want to hide it.
-    'developer_link' => 'http://tabacitu.ro',
+    'developer_link' => 'https://nilefalcon.com',
 
     // Show powered by Laravel Backpack in the footer? true/false
-    'show_powered_by' => true,
+    'show_powered_by' => false,
 
     // -------
     // SCRIPTS
